fe2303496feca5fdf7b6fe68f646f009098db215
tupegaurav <tupe.gaurav639@gmail.com>
2026-04-16 01:11:05 +0530

    Implement STL generation endpoint with OpenSCAD

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { exec } = require("child_process");
+const fs = require("fs");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/generate-stl", (req, res) => {
+  const cad_code = req.body.cad_code;
+
+  if (!cad_code) {
+    return res.status(400).send("No CAD code provided");
+  }
+
+  fs.writeFileSync("model.scad", cad_code);
+
+  exec("openscad -o output.stl model.scad", (err) => {
+    if (err) {
+      return res.status(500).send("OpenSCAD failed");
+    }
+
+    res.download("output.stl");
+  });
+});
+
+app.listen(5000, () => console.log("Server running"));
